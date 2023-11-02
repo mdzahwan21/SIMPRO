27c8f06cd3579ab9977b877b712a59dece794682
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-11-02 10:33:39 +0700

    add toggle-switch

--- resources/views/operator/generateAkun.blade.php
+++ resources/views/operator/generateAkun.blade.php
@@ -95,6 +95,13 @@
 
     <div class="p-4 sm:ml-64">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+
+            <label class="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" class="sr-only peer">
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">MAHASISWA</span>
+            </label>
+
             <h1 class="bg-blue-500 text-white text-center p-2 m-5 rounded">FORM AKUN MAHASISWA</h1>
             <form class="flex flex-col w-full" action="{{ route('skripsi') }}" enctype="multipart/form-data">
                 @csrf
