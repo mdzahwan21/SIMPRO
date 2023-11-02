d3e35401976dae75beddd476f7a6a525e1230a20
mitslina <mitslina.lina@gmail.com>
2023-11-02 14:24:13 +0700

    pkl bisa sv db

--- resources/views/Mahasiswa/pkl.blade.php
+++ resources/views/Mahasiswa/pkl.blade.php
@@ -9,8 +9,20 @@
         </div>
 
         <div class="flex p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-            <form class="flex flex-col w-full" action="{{ route('pkl.store') }}" enctype="multipart/form-data">
+            <form class="flex flex-col w-full" method="POST" action="{{ route('pkl.store') }}" enctype="multipart/form-data">
                 @csrf
+                @if (session('success'))
+                    <div class="p-4 bg-green-100 text-green-800 rounded-lg mb-4 text-center">
+                        {{ session('success') }} 
+                    </div>
+                @endif
+
+                @if (session('error'))
+                    <div class="p-4 bg-red-100 text-red-800 rounded-lg mb-4 text-center">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
                 <div class="flex justify-center items-center mb-6">
                     <div class="w-full max-w-md">
                         <label for="smt_aktif" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -24,20 +36,22 @@
                 <div class="flex justify-center items-center mb-6">
                     <div class="w-full max-w-md">
 
-                        <label for="status_PKL" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status PKL:</label>
+                        <label for="status_PKL" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status
+                            PKL:</label>
                         <select id="status_PKL" name="status_PKL"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-800 focus:border-blue-800 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option selected disabled>Pilih Status PKL</option>
-                            <option value="belum_PKL">Belum PKL</option>
-                            <option value="sedang_PKL">Sedang PKL</option>
-                            <option value="sudah_PKL">Sudah PKL</option>
+                            <option value="belum PKL">Belum PKL</option>
+                            <option value="sedang PKL">Sedang PKL</option>
+                            <option value="sudah PKL">Sudah PKL</option>
                         </select>
                     </div>
                 </div>
 
                 <div class="flex justify-center items-center mb-6">
                     <div class="w-full max-w-md">
-                        <label for="nilai" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Nilai PKL:</label>
+                        <label for="nilai" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Nilai
+                            PKL:</label>
                         <input type="number" id="nilai" name="nilai"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-800 focus:border-blue-800 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="masukkan jumlah sks" step="0.01" min="1" max="4">
