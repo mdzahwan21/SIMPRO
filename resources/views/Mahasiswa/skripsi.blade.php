0d27ad3118940a2ef118f14839584e24193c0c64
mitslina <mitslina.lina@gmail.com>
2023-11-09 15:57:55 +0700

    benerin pading mhs

--- resources/views/Mahasiswa/skripsi.blade.php
+++ resources/views/Mahasiswa/skripsi.blade.php
@@ -1,7 +1,7 @@
 @extends('Mahasiswa.navbar')
 
 @section('content')
-    <div class=" w-full space-y-2">
+    <div class=" w-full p-4 space-y-2">
         <div class="flex items-center justify-center p-4 bg-blue-800 rounded-lg">
             <p class="text-sm font-large text-white truncate dark:text-gray-300" role="none">
                 ENTRY DATA SKRIPSI
