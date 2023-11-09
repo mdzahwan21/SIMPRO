cdbe54ab575cae9cbcde3840a0d017c5f471e842
DFRST <hanyaseorangpelajarrr@gmail.com>
2023-11-09 18:33:31 +0700

    fix: user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $roles = ['dosen_wali', 'departemen', 'operator', 'mahasiswa'];
+        $role = $this->faker->randomElement($roles);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => strtolower($this->faker->firstName) . "@$role.com",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '12345',
             'remember_token' => Str::random(10),
+            'role' => $role,
         ];
     }
 
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+            ->count(10) // You can adjust the number of users you want to create
+            ->create();
+    }
+}
