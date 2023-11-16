687166362ed20dc205310c5bc17bac9b8448833e
mitslina <mitslina.lina@gmail.com>
2023-11-16 16:02:56 +0700

    push

--- resources/views/Mahasiswa/dashboard.blade.php
+++ resources/views/Mahasiswa/dashboard.blade.php
@@ -1,5 +1,15 @@
 @extends('Mahasiswa.navbar')
 
 @section('content')
+    <div class="w-full p-4 space-y-2">
+        
+    <div class="flex items-center gap-4">
+        <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="">
+        <div class="font-medium dark:text-white">
+            <div>Jese Leos</div>
+            <div class="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+        </div>
+    </div>
 
+    </div>
 @endsection
