a658f4722ea7fdb39476644067b6e15fc8dc8fa2
dellashanda <della.shanda@gmail.com>
2023-11-30 21:08:30 +0700

    baru

--- resources/views/doswal/verifikasiProgress.blade.php
+++ resources/views/doswal/verifikasiProgress.blade.php
@@ -2,26 +2,33 @@
 
 @section('content')
     <div class="w-full space-y-2">
-        <div class="position-fixed flex w-full p-3 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <div class="flex justify-center w-full gap-2 border-dashed border-gray-500">
-                <a href="/verifikasi-progress/list-IRS" class="nav-link-verifikasi">
-                    <button
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">IRS</button>
-                </a>
-                <a href="/verifikasi-progress/list-KHS" class="nav-link-verifikasi">
-                    <button
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">KHS</button>
-                </a>
-                <a href="/verifikasi-progress/list-PKL" class="nav-link-verifikasi">
-                    <button
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">PKL</button>
-                </a>
-                <a href="/verifikasi-progress/list-Skripsi" class="nav-link-verifikasi">
-                    <button
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">Skripsi</button>
-                </a>
-            </div>
-        </div>
+        <style>
+    .active {
+        background-color: #4caf50; /* Tambahkan warna latar belakang sesuai keinginan Anda */
+        color: white; /* Tambahkan warna teks yang sesuai */
+    }
+
+    .focus {
+        outline: none; /* Hilangkan outline default pada fokus jika tidak diinginkan */
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); /* Tambahkan shadow atau efek lainnya pada fokus */
+    }
+</style>
+
+<div class="position-fixed flex w-full p-1 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex justify-center w-full gap-2 border-dashed border-gray-500">
+        <a href="/irs" class="inputIrs">
+            <button class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 active focus">
+                <strong>Entry IRS</strong>
+            </button>
+        </a>
+        <a href="/irs/rekapIrs" class="rekapIrs">
+            <button class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 active focus">
+                <strong>Rekap IRS</strong>
+            </button>
+        </a>
+    </div>
+</div>
+
 
         <div class="w-full p-4">
             <div class="min-h-screen border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
